3532f23fb12fc6a0be255cfbe8b36ed76d2827f4
Vitalii Shevchuk <vitaliishevchuk@Vitaliis-MBP.attlocal.net>
2022-11-27 21:03:21 -0800

    initial implementation

--- src/App.tsx
+++ src/App.tsx
@@ -33,54 +33,6 @@ function App() {
     })
   }, [])
 
-  const handleScroll = (target: any, e: any) => {
-    const scrollTop = Math.round(target.scrollTop)
-    const clientHeight = Math.round(target.clientHeight)
-    const scrollHeight = Math.round(target.scrollHeight)
-    const rowHeight = Math.round(scrollHeight / rows.length)
-
-    if (isLoading) {
-      return
-    }
-
-    const isUp = scrollTop < currentScrollTop
-
-    if (isUp && scrollTop === 0) {
-      setIsLoading(true)
-
-      callApi(upperOffset - limit, limit).then((res: any) => {
-        const newRows = [...res, ...rows.slice(0, 200)] as any
-        setRows(newRows)
-        setIsLoading(false)
-        setUpperOffset(upperOffset - limit)
-        setLowerOffset(lowerOffset - limit)
-
-        if (myRef !== null) {
-          const scrollPos = (rows.length / 3) * rowHeight
-          myRef.current.scrollTo(0, scrollPos)
-        }
-      })
-    } else if (!isUp && scrollTop + clientHeight >= scrollHeight) {
-      console.log('next')
-      setIsLoading(true)
-      callApi(lowerOffset, limit).then((res: any) => {
-        const newRows = [...rows.slice(99, 299), ...res] as any
-
-        setRows(newRows)
-        setIsLoading(false)
-        setUpperOffset(upperOffset + limit)
-        setLowerOffset(lowerOffset + limit)
-
-        if (myRef !== null) {
-          const scrollPos = (rows.length / 3) * rowHeight
-          myRef.current.scrollTo(0, scrollPos * 2)
-        }
-      })
-    }
-
-    setCurrentScrollTop(scrollTop)
-  }
-
   const prevCallback = (newOffset: number) => {
     setIsLoading(true)
 
@@ -97,40 +49,18 @@ function App() {
 
     return callApi(newOffset, limit).then((res: any) => {
       const newRows = [...rows.slice(99, 299), ...res] as any
-
       setRows(newRows)
       setIsLoading(false)
       return true
     })
   }
 
-  const [isDataLoading, setIsDataLoading] = useState(false)
-
   return (
     <div className="App">
-      {/* <div
-        className="virtual-scroll"
-        style={{ height: '50vh', overflow: 'scroll' }}
-        ref={myRef}
-        onScroll={(e: any) => handleScroll(e.target, e)}
-      >
-        {rows.map((item: any, index: number) => (
-          <div style={{ padding: '10px' }}>
-            {isLoading ? <>Loading...</> : item}
-          </div>
-        ))}
-      </div> */}
-
-      {/* rowHeight: number
- 
- 
-  onPrevCallback: (newOffset: number) => void
-  onNextCallback: (newOffset: number) => void */}
-
       <UiVirtualScroll
         buffer={300}
         rowHeight={39}
-        height={50}
+        height="50vh"
         limit={100}
         onPrevCallback={prevCallback}
         onNextCallback={nextCallback}
